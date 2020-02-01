1e9cde4c31b2a856a12ac0500f49c58f22a7bd6d
secops4thewin <secops4thewin@users.noreply.github.com>
2020-02-02 07:34:22 +1100

    Update win_autoruns.cmd

--- bin/win_autoruns.cmd
+++ bin/win_autoruns.cmd
@@ -1,2 +1,2 @@
 @echo off
-cmd /c start /B "" "%SPLUNK_HOME%\etc\apps\TA_autoruns\bin\autorunsc.exe" -c -h -accepteula
+cmd /c start /B "" "%SPLUNK_HOME%\etc\apps\TA_autoruns\bin\autorunsc.exe" -c -h -accepteula -a *
